ced5e880a1f2464f624e5a45a110ab7d590621c5
Mohamed Said <mohamedsaid.2001@hotmail.com>
2022-09-26 13:56:38 +0200

    Started with the navbar

--- view/menu.php
+++ view/menu.php
@@ -1,1 +1,25 @@
-<?php
+<div class=" d-flex flex-column mb-2">
+
+    <div class="">
+        <ul class="px-0">
+            <div class="info p-2 align-items-center ">
+                <div class=" d-flex flex-row m-3">
+                    <div class="pe-3  ">
+                        <div class='c-avatar pt-1 '>
+                            <img class='c-avatar__image' src='https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80' alt=''>
+                            <span class='c-avatar__status'></span>
+                        </div>
+                    </div>
+                    <div class="p2">
+                        <a>Mohamed Said</a><br>
+                        <a>Titel</a>
+                    </div>
+                </div>
+            </div>
+
+            <li><a class="active" href="#home"><i class="icon" data-feather="home"></i> Dashboard</a></li>
+            <li><a href="#news"><i class="icon" data-feather="user"></i>Leden</a></li>
+            <li><a href="#contact"><i class="icon" data-feather="users"></i>Teams</a></li>
+            <li><a href="#about"><i class="icon" data-feather="user"></i>Klanten</a></li>
+        </ul>
+    </div>
