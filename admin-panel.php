9cb40cfa2c65a0c0b2c1739f66843ddb0706a080
Mohamed Said <mohamedsaid.2001@hotmail.com>
2022-09-26 09:26:55 +0200

    header almost finished

--- admin-panel.php
+++ admin-panel.php
@@ -9,8 +9,10 @@
         <title>Twirling Team Sparkle</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     </head>
-        <?php include('./view/header.php'); ?>
+
     <body>
+        <?php include('./view/header.php'); ?>
+
 
     </body>
     </html>
